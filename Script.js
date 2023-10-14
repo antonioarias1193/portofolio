7eb01c1ff56d2a0991be4ef889ca3fc0ba6b5846
Anton <147809331+antonioarias1193@users.noreply.github.com>
2023-10-15 01:02:14 +0700

    Add files via upload

--- Script.js
+++ Script.js
@@ -2,13 +2,13 @@ window.addEventListener("scroll", function () {
 	var gambar = document.querySelector(".gambar");
 	var container_fluid = document.querySelector(".container-fluid");
 
-	if (window.scrollY < 2) {
+	if (window.scrollY < 5) {
 		container_fluid.classList.add("m-5");
 	} else {
 		container_fluid.classList.remove("m-5");
 	}
 
-	if (window.scrollY < 2) {
+	if (window.scrollY < 5) {
 		gambar.classList.add("sizingonscroll");
 	} else {
 		gambar.classList.remove("sizingonscroll");
--- searchingApi.js
+++ searchingApi.js
@@ -1,52 +1,57 @@
 function searching_movie() {
-	// Getting Fetch API
-	const control = document.getElementById("search_control");
-	const Input = control.value;
+	// Fetch API
+	const control = document.getElementById("search_control"); // Selector to html ID search_control
+	const Input = control.value; // Get The search value
 	fetch("https://www.omdbapi.com/?apikey=68d923d9&s=" + Input)
 		.then((response) => response.json()) // Fetch API to Json File
 		.then((response) => {
 			Search_API(response); // Locate response data result to Data_API Function
 		})
 		.catch((err) => console.error(err)); // Get information error from API
+	//fetch API //
 
+	// Do processing data responses
 	function Search_API(response) {
-		const movies = response.Search;
-		let cards = "";
+		const movies = response.Search; // process response search results into movies variable
+		let cards = ""; // inisiate cards
 		movies.forEach((m) => {
-			cards += `<div class="col-md-4 my-5">
-            <div class="card">
-              <img src="${m.Poster}" class="card-img-top" alt= Movie "${m.Title}">
-              <div class="card-body">
-                <h5 class="card-title">${m.Title}</h5>
-                <h6 class="card-subtitle mb-2 text-body-secondary">${m.Year}</h6>
-                <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#details_movie" data-imdbid="${m.imdbID}">Details</a>
-              </div>
-            </div>
-          </div>`;
+			cards += showcards(m); // Show cards display based on the number of results
 		});
-		console.log(movies);
-		const paste = document.getElementById("Smovie_container");
-
-		// Mengubah inner HTML dengan teks baru
-		paste.innerHTML = cards;
+		console.log(movies); // Check results by console
+		const paste = document.getElementById("Smovie_container_cards");
+		paste.innerHTML = cards; // Put many cards into cards container
 
 		// function button details
-		const detailbt = document.querySelectorAll(".modal-detail-button");
+		const detailbt = document.querySelectorAll(".modal-detail-button"); // get button details value
 		detailbt.forEach((btn) => {
 			btn.addEventListener("click", function () {
-				const imdbid = this.dataset.imdbid; //Getting imdbid datas
+				const imdbid = this.dataset.imdbid; //Getting imdbid datas each clicked on single card details button
 
+				// Show information about Details
 				fetch("https://www.omdbapi.com/?apikey=68d923d9&i=" + imdbid)
 					.then((response) => response.json()) // Fetch API to Json File
 					.then((m) => {
-						const movieDetail = showMovieDetail(m); // Locate response data result to Data_API Function
+						const movieDetail = showMovieDetail(m); // puts details information into modal adjust imdbid
 						const modalBody = document.querySelector(".modal-body");
 						modalBody.innerHTML = movieDetail;
 					})
 					.catch((err) => console.error(err)); // Get information error from API
-			});
-		});
-	}
+			}); // Show information about Details //
+		}); // function button details //
+	} // Do processing data responses //
+}
+
+function showcards(m) {
+	return `<div class="col-md-4 my-5">
+	<div class="card">
+  		<img src="${m.Poster}" class="card-img-top" alt= Movie "${m.Title}">
+  		<div class="card-body">
+		<h5 class="card-title">${m.Title}</h5>
+		<h6 class="card-subtitle mb-2 text-body-secondary">${m.Year}</h6>
+		<a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#details_movie" data-imdbid="${m.imdbID}">Details</a>
+  		</div>
+	</div>
+</div>`;
 }
 
 function showMovieDetail(m) {
